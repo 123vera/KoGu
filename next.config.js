2fd20db0df3a1994620dd16103a53e3876153fab
123vera <v531141681@163.com>
2022-06-24 23:34:54 +0800

    ii

--- next.config.js
+++
@@ -1,8 +0,0 @@
-//  使用sass需此配置项
-const withSass = require("@zeit/next-sass")
-// module.exports = withSass();
-
-module.exports = {
-  withSass: withSass(), //  使用sass需此配置项
-  useFileSystemPublicRoutes: false // 禁止路由链接到/pages下的文件
-}
