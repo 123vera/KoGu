2278e8fec0b2c66102d289d0a7df28f029b2a8b7
wanghemeng <hemeng.wang@avatr.com>
2023-01-09 17:27:10 +0800

    fix

--- src/pages/javascript/home.js
+++ src/pages/javascript/home.js
@@ -1,12 +1,12 @@
-import React, { Component, useEffect, useState } from 'react';
-import "../../mock"
-import { downloadPdf } from '../../assets/utils/pdf'
-import ExportExcel from '../../components/common/Excel'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
-import RcCalendar from '../../components/common/RcCalendar'
-import DomToImage from '@components/common/DomToImage/index'
-import { convertToImage } from '../../assets/utils/convertToImage';
-import './home.scss'
+import React, { Component, useEffect, useState } from "react";
+import "../../mock";
+import { downloadPdf } from "../../assets/utils/pdf";
+import ExportExcel from "../../components/common/Excel";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import RcCalendar from "../../components/common/RcCalendar";
+import DomToImage from "../../components/common/DomToImage/index";
+import { convertToImage } from "../../assets/utils/convertToImage";
+import "./home.scss";
 // import { Clone, SearchUrl, Cookie, ArrFormat, NumFormat, DateFormat } from 'viki-npm'
 
 // 如果你在beforePopState中返回 false，Router将不会执行popstate事件。
@@ -23,10 +23,10 @@ import './home.scss'
 
 // 监听页面dom 节点增删，节点样式改变等
 function addObserverIfDesiredNodeAvailable(contents) {
-
-  const MutationObserver = window.MutationObserver || window.WebKitMutationObserver
+  const MutationObserver =
+    window.MutationObserver || window.WebKitMutationObserver;
   if (MutationObserver) {
-    let waterImageNode = document.querySelector('#waterImageContent');
+    let waterImageNode = document.querySelector("#waterImageContent");
 
     if (!waterImageNode) {
       window.setTimeout(addObserverIfDesiredNodeAvailable, 500);
@@ -34,23 +34,26 @@ function addObserverIfDesiredNodeAvailable(contents) {
     }
 
     var observerOptions = {
-      childList: true,  // 观察目标子节点的变化，是否有添加或者删除
+      childList: true, // 观察目标子节点的变化，是否有添加或者删除
       attributes: true, // 观察属性变动
-      subtree: true     // 观察后代节点，默认为 false
-    }
+      subtree: true, // 观察后代节点，默认为 false
+    };
 
     const observer = new MutationObserver(function (list, observer) {
       let text = {
-        userName: '王大王',
-        tele: 888
-      }
+        userName: "王大王",
+        tele: 888,
+      };
 
-      if ((waterImageNode && !waterImageNode.getAttribute('style').includes('background-image')) || !waterImageNode) {
-
+      if (
+        (waterImageNode &&
+          !waterImageNode.getAttribute("style").includes("background-image")) ||
+        !waterImageNode
+      ) {
         // 避免一直触发
         observer.disconnect();
-        observer = null
-        createCanvasToUrl(JSON.parse(JSON.stringify(text)))
+        observer = null;
+        createCanvasToUrl(JSON.parse(JSON.stringify(text)));
       }
     });
 
@@ -61,119 +64,121 @@ function addObserverIfDesiredNodeAvailable(contents) {
 // 水印canvas 转为 base64图片地址
 function createCanvasToUrl(contents) {
   let styles = {
-    width: '150px',
-    height: '150px',
-    textAlign: 'center',
-    textBaseline: 'middle',
+    width: "150px",
+    height: "150px",
+    textAlign: "center",
+    textBaseline: "middle",
     font: "10px microsoft yahei",
-    fillStyle: 'rgba(184, 184, 184, 0.8)',
-    rotate: '20',
+    fillStyle: "rgba(184, 184, 184, 0.8)",
+    rotate: "20",
     zIndex: 1000,
-  }
-  let { userName, tele } = contents
+  };
+  let { userName, tele } = contents;
 
-  var canvas = document.createElement('canvas');
-  canvas.setAttribute('width', styles.width);
-  canvas.setAttribute('height', styles.height);
+  var canvas = document.createElement("canvas");
+  canvas.setAttribute("width", styles.width);
+  canvas.setAttribute("height", styles.height);
   var ctx = canvas.getContext("2d");
 
-  ctx.textAlign = styles.textAlign
-  ctx.textBaseline = styles.textBaseline
+  ctx.textAlign = styles.textAlign;
+  ctx.textBaseline = styles.textBaseline;
   ctx.font = styles.font;
   ctx.fillStyle = styles.fillStyle;
-  ctx.rotate(Math.PI / 180 * styles.rotate);
-  ctx.fillText(userName + tele, parseFloat(styles.width) / 3, parseFloat(styles.height) / 3);
+  ctx.rotate((Math.PI / 180) * styles.rotate);
+  ctx.fillText(
+    userName + tele,
+    parseFloat(styles.width) / 3,
+    parseFloat(styles.height) / 3
+  );
 
   var base64Url = canvas.toDataURL();
-  let waterImageNode = document.getElementById('waterImageContent');
+  let waterImageNode = document.getElementById("waterImageContent");
 
-  waterImageNode.setAttribute('style', `background-image: url("${base64Url}")`)
-  addObserverIfDesiredNodeAvailable()
+  waterImageNode.setAttribute("style", `background-image: url("${base64Url}")`);
+  addObserverIfDesiredNodeAvailable();
 }
 
 // 水印文字
 function SetWaterImage() {
-
   let contents = {
-    userName: '王大王',
-    tele: 888
-  }
+    userName: "王大王",
+    tele: 888,
+  };
 
   useEffect(() => {
-    createCanvasToUrl(contents)
+    createCanvasToUrl(contents);
 
-    addObserverIfDesiredNodeAvailable(contents)
-  }, [document.getElementById('waterImageContent')])
+    addObserverIfDesiredNodeAvailable(contents);
+  }, [document.getElementById("waterImageContent")]);
 
   return (
-    <div id="waterImageContent" >
-      {/* <div className="waterImageContent" style={ { backgroundImage: 'url(' + createCanvasToUrl(contents) + ')' } }> */ }
+    <div id="waterImageContent">
+      {/* <div className="waterImageContent" style={ { backgroundImage: 'url(' + createCanvasToUrl(contents) + ')' } }> */}
       <ul>
         <li>第一步：准备水印文字（邮箱或用户名）</li>
         <li>第二步：文字转为canvas或svg</li>
         <li>第三步：用canvas 或svg 转为base64 图片</li>
         <li>第四步：以background-image插入base64 </li>
-
       </ul>
     </div>
-  )
+  );
 }
 
 // dom转canvas 输出pdf 下载
 function DomToPdf() {
   return (
-    <div id='pdfContainer'>
+    <div id="pdfContainer">
       <ul>
         <li>第一步：定义容器id和内容</li>
         <li>第二步：安装jsPDF —— new jsPDF()</li>
       </ul>
-      {/* <button onClick={ () => downloadPdf('888888', '文件2') }>下载</button> */ }
-    </div >
-  )
+      {/* <button onClick={ () => downloadPdf('888888', '文件2') }>下载</button> */}
+    </div>
+  );
 }
 
 class Home extends Component {
   state = {
     imageEl: null,
-    limitAmount: 10
-  }
+    limitAmount: 10,
+  };
 
   convert = () => {
-    convertToImage(document.getElementById('homeContainer'), { backgroundColor: null, size: 0.8 }).then(res => {
+    convertToImage(document.getElementById("homeContainer"), {
+      backgroundColor: null,
+      size: 0.8,
+    }).then((res) => {
       // convertToImage(document.getElementById('homeContainer'), {width: 100, height: 100,backgroundColor: null}).then(res => {
-      this.setState({ imageEl: res.src })
-    })
-  }
-
+      this.setState({ imageEl: res.src });
+    });
+  };
 
   render() {
     // console.log(Router.router && Router.router.pathname)  // 获取路由的方法
 
     return (
-      <div className='home' id="home">
-        <div id="homeContainer" >
-          <button onClick={ () => downloadPdf('pdfContainer', '文件2') }>（必须位于顶部）下载pdf</button>
+      <div className="home" id="home">
+        <div id="homeContainer">
+          <button onClick={() => downloadPdf("pdfContainer", "文件2")}>
+            （必须位于顶部）下载pdf
+          </button>
 
-          <a href="weixin://" >前往打开微信l</a>
+          <a href="weixin://">前往打开微信l</a>
 
-
-          <fieldset  >
+          <fieldset>
             <legend>—— 复制粘贴功能 ——</legend>
             <CopyToClipboard
-              style={ { cursor: 'pointer' } }
-              text={ '这是一段复制内容' }
-              onCopy={ () => alert('复制成功') }
+              style={{ cursor: "pointer" }}
+              text={"这是一段复制内容"}
+              onCopy={() => alert("复制成功")}
             >
-              <span>{ '复制地址' }</span>
+              <span>{"复制地址"}</span>
             </CopyToClipboard>
-
           </fieldset>
 
-
-
-          {/* <img className="testIng" src={ test1 } alt="" /> */ }
-          {/* cdn图片不显示因为跨域问题，待研究 */ }
-          {/* <img className="testIng" src="https://cdn.cnviinet.com/viinet-app-web-v2/static/linkafeiquan-202006101045.jpg" alt=""/>  */ }
+          {/* <img className="testIng" src={ test1 } alt="" /> */}
+          {/* cdn图片不显示因为跨域问题，待研究 */}
+          {/* <img className="testIng" src="https://cdn.cnviinet.com/viinet-app-web-v2/static/linkafeiquan-202006101045.jpg" alt=""/>  */}
         </div>
 
         {/* <div id="eee">
@@ -183,57 +188,49 @@ class Home extends Component {
         </div> */}
 
         <div>
-
-          {/* 以下解决的问题是：dom转化为canvas ，再转为图片，并下载或保存，主要方法在 utils/convertToImage 文件*/ }
-          <fieldset  >
-            <div >
+          {/* 以下解决的问题是：dom转化为canvas ，再转为图片，并下载或保存，主要方法在 utils/convertToImage 文件*/}
+          <fieldset>
+            <div>
               <legend>—— 下载图片 ——</legend>
-              <button onClick={ this.convert }>下载图片</button>
+              <button onClick={this.convert}>下载图片</button>
               <br />
-              {/* <img src={ this.state.imageEl } alt="" /> */ }
+              {/* <img src={ this.state.imageEl } alt="" /> */}
             </div>
-
           </fieldset>
 
-
-          {/* 实现水印 */ }
+          {/* 实现水印 */}
           <fieldset>
             <legend>—— 实现删不掉的水印 ——</legend>
             <SetWaterImage />
           </fieldset>
 
-
-          {/* dom转pdf下载 */ }
+          {/* dom转pdf下载 */}
           <fieldset>
             <legend>—— dom转pdf下载 ——</legend>
             <DomToPdf />
           </fieldset>
 
-
-          {/* dom导出图片 */ }
+          {/* dom导出图片 */}
           <fieldset>
-            <legend>—— dom导出图片  ——</legend>
-            < DomToImage />
+            <legend>—— dom导出图片 ——</legend>
+            <DomToImage />
           </fieldset>
 
-          {/* 下载excel */ }
+          {/* 下载excel */}
           <fieldset>
-            <legend>——下载excel  ——</legend>
+            <legend>——下载excel ——</legend>
             <ExportExcel />
           </fieldset>
 
-
-          {/* 日历组件 */ }
+          {/* 日历组件 */}
           <fieldset>
-            <legend>——日历组件  ——</legend>
-            < RcCalendar />
+            <legend>——日历组件 ——</legend>
+            <RcCalendar />
           </fieldset>
-
-
         </div>
-      </div >
-    )
+      </div>
+    );
   }
 }
 
-export default Home
+export default Home;
--- src/routes/config.jsx
+++ src/routes/config.jsx
@@ -1,38 +1,38 @@
 
 const routes = [
-    { key: '/', title: 'home', component: require('@pages/catalogue/').default },
+    { key: '/', title: 'home', component: require('@/pages/catalogue/').default },
 
     // react
-    { key: '/rc/hook', title: 'hook应用', component: require('@pages/javascript/reactHook/').default },
-    { key: '/rc/react-ref', title: 'react-ref', component: require('@pages/javascript/reactJs/senior/ref/').default },
-    { key: '/rc/react-context', title: 'react-context', component: require('@pages/javascript/reactJs/senior/context/').default },
-    { key: '/rc/react-source', title: 'React源码', component: require('@pages/javascript/reactJs/reactSource/').default },
-    { key: '/rc/react-senior', title: 'React高级', component: require('@pages/javascript/reactJs/reactSenior/').default },
+    { key: '/rc/hook', title: 'hook应用', component: require('@/pages/javascript/reactHook/').default },
+    { key: '/rc/react-ref', title: 'react-ref', component: require('@/pages/javascript/reactJs/senior/ref/').default },
+    { key: '/rc/react-context', title: 'react-context', component: require('@/pages/javascript/reactJs/senior/context/').default },
+    { key: '/rc/react-source', title: 'React源码', component: require('@/pages/javascript/reactJs/reactSource/').default },
+    { key: '/rc/react-senior', title: 'React高级', component: require('@/pages/javascript/reactJs/reactSenior/').default },
 
 
     // threejs
-    { key: '/threejs/line', title: 'threejs - 模拟线', component: require('@pages/threejs/line/').default },
-    { key: '/threejs/curve', title: 'threejs 一些尝试', component: require('@pages/threejs/curve/').default },
-    { key: '/threejs/interLine', title: 'threejs - demo', component: require('@pages/threejs/interLine/').default },
-    { key: '/threejs/wormHoles', title: 'threejs - 虫洞', component: require('@pages/threejs/wormHoles/').default },
+    { key: '/threejs/line', title: 'threejs - 模拟线', component: require('@/pages/threejs/line/').default },
+    { key: '/threejs/curve', title: 'threejs 一些尝试', component: require('@/pages/threejs/curve/').default },
+    { key: '/threejs/interLine', title: 'threejs - demo', component: require('@/pages/threejs/interLine/').default },
+    { key: '/threejs/wormHoles', title: 'threejs - 虫洞', component: require('@/pages/threejs/wormHoles/').default },
 
     // javascript
-    { key: '/js/inter', title: '算法题', component: require('@components/hook/Interview/').default },
-    { key: '/js/js', title: 'javascript练习', component: require('@pages/javascript/Algorithm/jsTest1/').default },
-    { key: '/js/scrollreveal', title: '双向滚动页面', component: require('@pages/html/scrollreveal/').default },
-    { key: '/js/worldArea', title: '世界国家城市联动', component: require('@pages/html/worldArea/').default },
-    { key: '/js/cloneDeep', title: ' 深拷贝和浅拷贝', component: require('@pages/javascript/cloneDeep/').default },
-    { key: '/js/home', title: ' 最佳实践', component: require('@pages/javascript/home/').default },
-    { key: '/js/es11', title: 'ES11', component: require('@components/common/Es/').default },
+    { key: '/js/inter', title: '算法题', component: require('@/components/hook/Interview/').default },
+    { key: '/js/js', title: 'javascript练习', component: require('@/pages/javascript/Algorithm/jsTest1/').default },
+    { key: '/js/scrollreveal', title: '双向滚动页面', component: require('@/pages/HTML/scrollreveal/').default },
+    { key: '/js/worldArea', title: '世界国家城市联动', component: require('@/pages/HTML/worldArea/').default },
+    { key: '/js/cloneDeep', title: ' 深拷贝和浅拷贝', component: require('@/pages/javascript/cloneDeep/').default },
+    { key: '/js/home', title: ' 最佳实践', component: require('@/pages/javascript/home/').default },
+    { key: '/js/es11', title: 'ES11', component: require('@/components/common/Es/').default },
 
     // css & html
-    { key: '/html/css', title: '不常用的CSS属性1', component: require('@pages/css/index/').default },
-    { key: '/html/testcss', title: '不常用的CSS属性2', component: require('@pages/css/test/').default },
-    { key: '/html/htmlTag', title: '不常用的html标签', component: require('@components/common/HtmlTag/').default },
-    { key: '/html/inputItem', title: '不常用的表单元素', component: require('@components/common/InputItem/').default },
+    { key: '/html/css', title: '不常用的CSS属性1', component: require('@/pages/CSS/index/').default },
+    { key: '/html/testcss', title: '不常用的CSS属性2', component: require('@/pages/CSS/test/').default },
+    { key: '/html/htmlTag', title: '不常用的html标签', component: require('@/components/common/HtmlTag/').default },
+    { key: '/html/inputItem', title: '不常用的表单元素', component: require('@/components/common/InputItem/').default },
 
     // network
-    { key: '/network', title: '浏览器', component: require('@pages/network/index/').default },
+    { key: '/network', title: '浏览器', component: require('@/pages/network/index/').default },
 
 ]
 
